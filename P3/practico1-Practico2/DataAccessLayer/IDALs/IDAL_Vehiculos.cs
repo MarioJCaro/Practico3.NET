17fe465b0438d8515e8024b7f3b95eda626df654
MarioJCaro <mario.caro@estudiantes.utec.edu.uy>
2023-09-13 21:42:17 -0300

    Entrega Practico 3 Mario Caro

---
+++ P3/practico1-Practico2/DataAccessLayer/IDALs/IDAL_Vehiculos.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.EFModels;
+using Shared;
+
+namespace DataAccessLayer.IDALs
+{
+    public interface IDAL_Vehiculos
+    {
+        void Delete(string matricula);
+        List<Vehiculo> Get();
+        Vehiculo Get(string matricula);
+        void Insert(Vehiculo vehiculo, string documentoPersona);
+        void Update(Vehiculo vehiculo);
+    }
+}
